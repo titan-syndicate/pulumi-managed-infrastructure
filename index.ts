70b4387e09493a0bf24b4a69e0dfb1ef437a086b
Rian Fowler <rian@replicated.com>
2022-09-20 10:52:32 -0500

    make exportable

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as linode from "@pulumi/linode";
 
 // Create a Linode resource (Linode Instance)
-const instance = new linode.Instance("my-instance", {
-    type: "g6-nanode-1",
-    region: "us-east",
-    image: "linode/ubuntu18.04",
-});
+// const instance = new linode.Instance("my-instance", {
+//     type: "g6-nanode-1",
+//     region: "us-east",
+//     image: "linode/ubuntu18.04",
+// });
 
 // Export the Instance label of the instance
-export const instanceLabel = instance.label;
+// export const instanceLabel = instance.label;
+
+function createLinodeInstance(label: string = 'my-instance', type: string = 'g6-nanode-1', region: string = 'us-east', image: string = 'linode/ubuntu18.04') {
+    return new linode.Instance(label, {
+        type: type,
+        region: region,
+        image: image,
+    });
+}
+
+export { createLinodeInstance };
